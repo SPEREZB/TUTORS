5dbeecc504d715479c994f94039a2a2cbcc7c434
SebasPBR18 <sperezb@uteq.edu.ec>
2023-08-26 16:46:30 -0500

    first commit

--- app/src/lib/ui/legacy/Molecules/Cards/Card.svelte
+++ app/src/lib/ui/legacy/Molecules/Cards/Card.svelte
@@ -6,6 +6,27 @@
   import { onDestroy } from "svelte";
   import { Image } from "$lib/ui/legacy";
   import { cardTransition } from "$lib/ui/animations";
+  import { onMount } from "svelte";
+
+  let offsetX = 0;
+  let offsetY = 0;
+  const maxOffset = 10; // Cambia esto para ajustar la cantidad de parallax
+
+  const handleMouseMove = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    offsetX = ((event.clientX - rect.left - rect.width / 2) / rect.width) * maxOffset;
+    offsetY = ((event.clientY - rect.top - rect.height / 2) / rect.height) * maxOffset;
+  };
+
+  const resetOffset = () => {
+    offsetX = 0;
+    offsetY = 0;
+  };
+
+  onMount(() => {
+    const card = document.querySelector(".card");
+    card.addEventListener("mouseleave", resetOffset);
+  });
 
   export let lo: Lo;
   let target = "";
@@ -40,12 +61,12 @@
 <a href={lo.route} {target}>
   <div
     transition:cardTransition
-    class="card !bg-surface-50 dark:!bg-surface-700 border-y-8 border-{getIcon(lo.type)
+    class="card !bg-green-50 dark:!bg-surface-700 border-{getIcon(lo.type)
       .colour}-500 m-2 {cardWidths} transition-all hover:scale-105"
   >
     <header class="card-header flex flex-row items-center justify-between p-3">
       <div class="inline-flex w-full">
-        <div class="line-clamp-2 flex-auto {headingText} !text-black dark:!text-white">
+        <div class="text-center line-clamp-2 flex-auto {headingText} !text-black dark:!text-white">
           {lo.title}
         </div>
         {#if $currentCourse && !$currentCourse.areVideosHidden()}
